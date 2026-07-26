678b6fccaadf948c60f9bf631de6062bb4eab1f3
h28bi <h28bi@uwaterloo.ca>
2026-07-25 23:49:58 -0400

    Implement initial game controller commands

---
+++ src/gamecontroller.cc
@@ -0,0 +1,46 @@
+module;
+
+#include <string>
+
+export module gamecontroller;
+
+import board;
+import builder;
+import dice;
+import types;
+
+export class GameController {
+  private:
+    Board board;
+
+    FairDice fairDice;
+    LoadedDice loadedDice;
+
+    Builder blueBuilder;
+    Builder redBuilder;
+    Builder orangeBuilder;
+    Builder yellowBuilder;
+
+    Builder *builders[4];
+
+    int currentTurn;
+    bool gameRunning;
+
+    Builder &getCurrentBuilder();
+
+  public:
+    GameController();
+
+    void run();
+    void startNewGame();
+    void processCommand(const std::string &command);
+    void nextTurn();
+
+    bool handleTrade(
+        Builder &other,
+        ResourceType give,
+        ResourceType take
+    );
+
+    bool hasWinner() const;
+};
